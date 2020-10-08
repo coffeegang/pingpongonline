b663f6f1cfae17e0aa7a600641d173f28b645c9b
TexLine <cawaper@mail.ru>
2020-10-08 18:09:16 +0500

    Создали счет

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame 
 import ball
 import rockets
-
+import time
 
 # init
 pygame.init ()
@@ -14,7 +14,6 @@ screen_rect = screen.get_rect()
 run = True
 
 
-
 # objects
 all_objects = pygame.sprite.Group()
 ball = ball.Ball(weight/2, height/2, screen_rect)
@@ -24,9 +23,17 @@ score = [0, 0]
 
 
 # func for obj drawing
-def draw():
+def draw(): 
     screen.fill ((0,0,0))
     all_objects.draw(screen)
+    draw_text(screen, str(score[0]), 20, screen_rect.right/2, 5)
+font_name = pygame.font.match_font('arial')
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, (255,255,255))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
 
 
 # updating func
@@ -40,10 +47,11 @@ def update():
 while run:
     for event in pygame.event.get ():
         if event.type == pygame.QUIT:
-            run=False
+            run = False
         if event.type == pygame.MOUSEMOTION:
             pos = pygame.mouse.get_pos([1])
             rocket_right.y = pos[1]
+
     draw()
     update()
     
