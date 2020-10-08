374385f6c08bceb8f5eec330008a12bae762fb30
TexLine <cawaper@mail.ru>
2020-10-08 14:34:54 +0500

    icon

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame 
 pygame.init ()
-screen = pygame.display.set_mode ((800, 600))
+pygame.display.set_caption('PingPongOnline')
+icon = pygame.image.load('images/icon.png')
+pygame.display.set_icon(icon)
+size = weight, height = 1024, 768
+screen = pygame.display.set_mode (size)
 run = True
 while run:
     for event in pygame.event.get ():
         if event.type == pygame.QUIT:
             run=False
-    screen.fill ((0,0,0))
+    screen.fill ((0,0,0)) 
     pygame.display.flip ()
